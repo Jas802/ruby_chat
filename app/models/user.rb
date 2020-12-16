00574add4c693a79450b0c59015210540e7910ba
Jas802 <jas.spencer414@gmail.com>
2020-12-16 14:59:14 -0500

    added authentication methods to controller and menu

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-  validates: :username, presence: true, length: { minimum: 3, maxinum: 15},
+  validates :username, presence: true, length: { minimum: 3, maxinum: 15},
           uniqueness: { case_sensative: false }
   has_many :messages
-  has_secured_password
+  has_secure_password
 end
